f6f4836a07dcebd0ef7bd59814b12f698ebd76f0
Nithilan <53109782+c00p4rklynn@users.noreply.github.com>
2022-01-24 15:50:07 +0530

    Create py2.py

---
+++ 30 days of code/py2.py
@@ -0,0 +1,12 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables. 
+a = int(input())
+b = float(input())
+c = input()
+# Read and save an integer, double, and String to your variables.
+a = a + i
+print(a)
+print(d + b)
+print(s + c+'\n')
