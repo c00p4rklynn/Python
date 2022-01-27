00fdeed190abcdb057696e1e48125416c8a21232
Nithilan <53109782+c00p4rklynn@users.noreply.github.com>
2022-01-27 10:53:26 +0530

    Create py8arrays.py
    Task
    Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.

---
+++ 30 days of code/py8arrays.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    
+    i = 0
+    for i in range(n) :
+        print(str(arr[n-i-1]))
