ad7c84ec1f93f7d00dd038c145870de29c7baaac
Nithilan <53109782+c00p4rklynn@users.noreply.github.com>
2022-01-30 18:46:37 +0530

    Update py1.py

--- data structures/py1.py
+++ data structures/py1.py
@@ -1,46 +1,24 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
 import sys
 
-#
-# Complete the 'hourglassSum' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts 2D_INTEGER_ARRAY arr as parameter.
-#
 
-def hourglassSum(arr):
-    # Write your code here
-    i = 0
-    max = 0
-    for i in list(range(4)):
-        j = 0
-        for j in list(range(4)):
-            sum = arr[i][j] + arr[i+1][j] + arr[i+2][j]
-            print (sum)
-            sum = arr[i+1][j+1]
-            sum = arr[i][j+2] + arr[i+1][j+2] + arr[i+2][j+2]
-            print (sum)
-            if sum > max :
-                max = sum
-                
-    print(max)
-        
+arr = []
+for arr_i in range(6):
+    arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+    arr.append(arr_t)
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    arr = []
-
-    for _ in range(6):
-        arr.append(list(map(int, input().rstrip().split())))
-
-    result = hourglassSum(arr)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    
+best = 0
+for ind_i in range(6-2):
+    for ind_j in range(6-2):
+        temp = 0
+        temp += arr[ind_i][ind_j] + arr[ind_i][ind_j+1] + arr[ind_i][ind_j+2]
+        temp += arr[ind_i+1][ind_j+1]
+        temp += arr[ind_i+2][ind_j] + arr[ind_i+2][ind_j+1] + arr[ind_i+2][ind_j+2]
+        if ind_i == 0 and ind_j == 0:
+            best = temp
+        else:
+            best = max(best, temp)
+    
+print(best)
